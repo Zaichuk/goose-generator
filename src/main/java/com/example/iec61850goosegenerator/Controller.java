c98ebb5a499b1f67ea291c8c88e7f9759b251ff6
Ivan Zaichuk <zaichukip516@gmail.com>
2023-06-07 11:52:05 +0300

    добавил метод отправки пакетов при изменении TextFild

--- src/main/java/com/example/iec61850goosegenerator/Controller.java
+++ src/main/java/com/example/iec61850goosegenerator/Controller.java
@@ -4,11 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-import java.lang.reflect.Field;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Controller {
@@ -46,10 +42,10 @@ public class Controller {
     private TextField data7;
     private SendingPackets sendingPacket = new SendingPackets();
     ScheduledExecutorService steadySendingThread = Executors.newSingleThreadScheduledExecutor();
-    ScheduledExecutorService transitionSendingThread = Executors.newSingleThreadScheduledExecutor();
+//    ScheduledExecutorService transitionSendingThread = Executors.newSingleThreadScheduledExecutor();
 
     private ScheduledFuture steadySendingTask;
-    private ScheduledFuture transitionSendingTask;
+    private Future<?> transitionSendingTask;
     //    private AtomicInteger stNumForSending = new AtomicInteger(0);
     private AtomicInteger sqNumForSending = new AtomicInteger(0);
 
@@ -61,9 +57,11 @@ public class Controller {
 
 
         setGoosePacketByTextFields();
-        AtomicInteger stNumForSending = new AtomicInteger(0);
+        AtomicInteger sqNumForSending = new AtomicInteger(0);
 
-        goosePacket.setStNum(stNumForSending.get());
+        goosePacket.setStNum(sqNumForSending.get());
+
+        /*сделать проверку на null чтобы только один раз иницаилизировать*/
 
         sendingPacket.setNicName("VirtualBox Host-Only Ethernet Adapter" );
 
@@ -71,8 +69,8 @@ public class Controller {
         if (steadySendingTask == null) {
             steadySendingTask = steadySendingThread.scheduleWithFixedDelay(() -> {
                 sendingPacket.sendPackets(goosePacket);
-                stNumForSending.incrementAndGet();
-                goosePacket.setStNum(stNumForSending.get());
+                sqNumForSending.incrementAndGet();
+                goosePacket.setSqNum(sqNumForSending.get());
 
 
             }, 1, 2, TimeUnit.SECONDS);
@@ -88,21 +86,55 @@ public class Controller {
 
     private void setGoosePacketByTextFields() {
 
-        TextField[] textFieldsArray = new TextField[]{macDst, macSrc, gocbRef, datSet, goID, data, data1, data2, data3, data4, data5, data6, data7};
-        Class<? extends GoosePacket> aClass = goosePacket.getClass();
-        Field[] declaredFields = aClass.getDeclaredFields();
-
-
-
-        for (int i = 0; i < textFieldsArray.length; i++) {
-            textFieldsArray[i].textProperty().addListener((observable, oldValue, newValue) -> {
-
-
-
-                    }
-
-            );
-        }
+//        TextField[] textFieldsArray = new TextField[]{macDst, macSrc, gocbRef, datSet, goID, data, data1, data2, data3, data4, data5, data6, data7};
+//        Class<? extends GoosePacket> aClass = goosePacket.getClass();
+//        Field[] declaredFields = aClass.getDeclaredFields();
+//
+//
+//        for (int i = 0; i < textFieldsArray.length; i++) {
+//            int finalI = i;
+//            textFieldsArray[i].textProperty().addListener((observable, oldValue, newValue) -> {
+//
+//                Field field = declaredFields[finalI];
+//                if (textFieldsArray[finalI].getText().equals(field.getName())) {
+//                    field.setAccessible(true);
+//
+//                    if (field.getType().getSimpleName().equals(int.class.getSimpleName())) {
+//                        try {
+//                            /* тип new value и тип поля goosePacket совпадают?*/
+//
+//                            field.set(goosePacket, Integer.valueOf(newValue));
+//                        } catch (IllegalAccessException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    } else if (field.getType().getSimpleName().equals(String.class.getSimpleName())) {
+//
+//                        try {
+//                            /* тип new value и тип поля goosePacket совпадают?*/
+//
+//                            field.set(goosePacket, newValue);
+//                        } catch (IllegalAccessException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//
+//                    }else {
+//                        try {
+//                            /* тип new value и тип поля goosePacket совпадают?*/
+//
+//                            field.set(goosePacket, Boolean.valueOf(newValue));
+//                        } catch (IllegalAccessException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//
+//
+//                }
+//
+//
+//                    }
+//
+//            );
+//        }
         /*я хочу чтобы по изменеию новое значени добавлялосмь в пакет и запускался поток */
         goosePacket.setMacDst(macDst.getText());
         goosePacket.setMacSrc(macSrc.getText());
@@ -111,21 +143,54 @@ public class Controller {
         goosePacket.setTimeAllowedtoLive(4805);
         goosePacket.setDatSet(datSet.getText());
         goosePacket.setGoID(goID.getText());
-        // goosePacket.setStNum(0);
-        goosePacket.setSqNum(0);
+         goosePacket.setStNum(0);
+        goosePacket.setStNum(0);
 
 
         goosePacket.setSimulation(Boolean.getBoolean(simulation.getText()));
         goosePacket.setConfRef(Integer.valueOf(confRef.getText()));
         goosePacket.setNdsCom(Boolean.getBoolean(ndsCom.getText()));
 
+        /*тут соит сетить поля data в goosePacket*/
+
         boolean[] data = {Boolean.getBoolean(this.data.getText()), Boolean.getBoolean(data1.getText()), Boolean.getBoolean(data2.getText()), Boolean.getBoolean(data3.getText()), Boolean.getBoolean(data4.getText()), Boolean.getBoolean(data5.getText()), Boolean.getBoolean(data6.getText()), Boolean.getBoolean(data7.getText())};
 
         goosePacket.setNumDatSetEntries(8);
         goosePacket.setAllData(data);
 
+        confRef.textProperty().addListener((observable, oldValue, newValue) -> {
+            goosePacket.setConfRef(Integer.valueOf(newValue));
+            startTransitionSending();
+                }
+
+        );
+
 
     }
+
+    private void startTransitionSending(){
+        steadySendingTask.cancel(true);
+        steadySendingTask = null;
+
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + 2000;
+        int executorCount = 2;
+        if (transitionSendingTask == null) {
+            while (System.currentTimeMillis() < endTime) {
+
+                ExecutorService transitionSendingExecutors = Executors.newFixedThreadPool(executorCount);
+                transitionSendingTask = transitionSendingExecutors.submit(() -> sendingPacket.sendPackets(goosePacket));
+                executorCount *= 2;
+            }
+            transitionSendingTask.cancel(true);
+            transitionSendingTask = null;
+
+
+
+        }
+
+    }
+
 
 
 
