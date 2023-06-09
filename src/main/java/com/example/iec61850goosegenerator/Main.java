3f173cf1cb04d1f4c522573ed60e64fb51d8adba
Ivan Zaichuk <zaichukip516@gmail.com>
2023-06-10 00:01:20 +0300

    изменил размер сцены

--- src/main/java/com/example/iec61850goosegenerator/Main.java
+++ src/main/java/com/example/iec61850goosegenerator/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("goose-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 821, 527);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 480);
         stage.setTitle("Goose Generator");
         stage.setScene(scene);
         stage.show();
